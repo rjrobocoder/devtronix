75e73cf7233b09930200a35d7d36873d4d271c0e
rjbiswas <rajesh.biswas@nettantra.net>
2023-03-27 18:46:16 +0530

    Added development component

---
+++ components/Development.jsx
@@ -0,0 +1,12 @@
+const Development = () => {
+    return (
+        <div className="absolute w-[144px] top-[30px] left-[50px]">
+            <img
+                src="assets/work-in-progress.png"
+                alt="work in progress"
+            />
+        </div>
+    );
+}
+
+export default Development;
